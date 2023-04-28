05586dd99d5ab4f33169bfc1998480e55938472d
nicole gumula <ngumula@guilford.edu>
2023-04-28 18:15:19 -0400

    project4

---
+++ src/main/java/edu/guilford/PositionApplicant.java
@@ -0,0 +1,196 @@
+package edu.guilford;
+
+public class PositionApplicant {
+    // attributes: These are what we want each Life object to know about itself
+
+    private int applicantNumber;
+    private int introCompProg;
+    private int advCompProg;
+    private int operatingSystems; 
+    private int networking; 
+    private int databaseSystems; 
+    private int algorithms; 
+     
+
+    // constructor
+    public PositionApplicant(int applicantNumber, int introCompProg, int advCompProg, int operatingSystems, int networking, int databaseSystems, int algorithms) {
+        this.applicantNumber = applicantNumber;
+        this.introCompProg = introCompProg;
+        this.advCompProg = advCompProg;
+        this.operatingSystems = operatingSystems;
+        this.networking = networking;
+        this.databaseSystems = databaseSystems;
+        this.algorithms = algorithms;
+        
+    }
+
+    // getters and setters
+   
+    public int getApplicantNumber() {
+        return applicantNumber;
+    }
+
+    public int getIntroCompProg() {
+        return introCompProg;
+    }
+
+    public int getAdvCompProg() {
+        return advCompProg;
+    }
+
+    public int getOperatingSystems() {
+        return operatingSystems;
+    }
+
+    public int getNetworking() {
+        return networking;
+    }
+
+    public int getDatabaseSystems() {
+        return databaseSystems;
+    }
+
+    public int getAlgorithms() {
+        return algorithms;
+    }
+
+    public void setApplicantNumber(int applicantNumber) {
+        this.applicantNumber = applicantNumber;
+    }
+
+    public void setIntroCompProg(int introCompProg) {
+        this.introCompProg = introCompProg;
+    }
+
+    public void setAdvCompProg(int advCompProg) {
+        this.advCompProg = advCompProg;
+    }
+
+    public void setOperatingSystems(int operatingSystems) {
+        this.operatingSystems = operatingSystems;
+    }
+
+    public void setNetworking(int networking) {
+        this.networking = networking;
+    }
+
+    public void setDatabaseSystems(int databaseSystems) {
+        this.databaseSystems = databaseSystems;
+    }
+
+    public void setAlgorithms(int algorithms) {
+        this.algorithms = algorithms;
+    }
+
+    //add a static quickSort method that uses an array of PositionApplicant as a parameter
+    //and sorts the array in quicksort
+    public static void quickSort(int[] applicants) {
+        quickSort(applicants, 0, applicants.length - 1);
+    }
+
+    // add a selection sort method that uses an array of integers  as a parameter
+    // and sorts the array in selection sort
+    public static void selectionSort(int[] applicants) {
+        // loop through the array
+        for (int i = 0; i < applicants.length; i++) {
+            // find the smallest element in the array
+            int smallest = i;
+            for (int j = i + 1; j < applicants.length; j++) {
+                if (applicants[j] < applicants[smallest]) {
+                    smallest = j;
+                }
+            }
+            // swap the smallest element with the first element in the array
+            int temp = applicants[i];
+            applicants[i] = applicants[smallest];
+            applicants[smallest] = temp;
+        }
+    }
+
+    // add a static quicksort method that uses an array of integers as a parameter
+    // and sorts the array in quicksort
+    public static void quickSort(int[] applicants, int start, int end) {
+        // if the array has more than one element
+        if (start < end) {
+            // partition the array
+            int partitionIndex = partition(applicants, start, end);
+            // sort the left side of the array
+            quickSort(applicants, start, partitionIndex - 1);
+            // sort the right side of the array
+            quickSort(applicants, partitionIndex + 1, end);
+        }
+    }
+
+    // add a static partition method that uses an array of integers as a parameter
+    // and returns the partition index
+    public static int partition(int[] applicants, int start, int end) {
+        // set the pivot to the last element in the array
+        int pivot = applicants[end];
+        // set the partition index to the first element in the array
+        int partitionIndex = start;
+        // loop through the array
+        for (int i = start; i < end; i++) {
+            // if the current element is less than or equal to the pivot
+            if (applicants[i] <= pivot) {
+                // swap the current element with the element at the partition index
+                int temp = applicants[i];
+                applicants[i] = applicants[partitionIndex];
+                applicants[partitionIndex] = temp;
+                // increment the partition index
+                partitionIndex++;
+            }
+        }
+        // swap the element at the partition index with the pivot
+        int temp = applicants[partitionIndex];
+        applicants[partitionIndex] = applicants[end];
+        applicants[end] = temp;
+        // return the partition index
+        return partitionIndex;
+    }
+
+
+    // add a PositionApplicant sort method using linear search 
+    public static int linearSearch(int[] applicants, int applicantNumber) {
+        // loop through the array
+        for (int i = 0; i < applicants.length; i++) {
+            // if the current element is equal to the value we are searching for, return the index
+            if (applicants[i] == applicantNumber) {
+                return i;
+            }
+        }
+        // if we get here, the value was not found, so return -1
+        return -1;
+    }
+
+    //add a binarySearch algorithm
+    public static int binarySearch(int[] applicants, int applicantNumber) {
+        // set the start index to 0
+        int start = 0;
+        // set the end index to the length of the array minus 1
+        int end = applicants.length - 1;
+        // while the start index is less than or equal to the end index
+        while (start <= end) {
+            // set the middle index to the average of the start and end indices
+            int middle = (start + end) / 2;
+            // if the value at the middle index is equal to the value we are searching for, return the index
+            if (applicants[middle] == applicantNumber) {
+                return middle;
+            }
+            // if the value at the middle index is less than the value we are searching for, set the start index to the middle index plus 1
+            else if (applicants[middle] < applicantNumber) {
+                start = middle + 1;
+            }
+            // if the value at the middle index is greater than the value we are searching for, set the end index to the middle index minus 1
+            else {
+                end = middle - 1;
+            }
+        }
+        // if we get here, the value was not found, so return -1
+        return -1;
+    }
+
+
+
+
+
+}
---
+++ src/main/java/edu/guilford/Sort.java
@@ -0,0 +1,84 @@
+package edu.guilford;
+
+
+import java.util.Arrays;
+import java.util.Random;
+import java.util.Scanner;
+
+/**
+ * Hello world!
+ *
+ */
+public class Sort 
+{
+    public static void main( String[] args ) {
+        final int MAX; 
+        final int RANGE = 100;
+        Random rand = new Random(); 
+
+        // ask the user how many objects do they want to create
+        System.out.println("How many applicants do you want to create?");
+        Scanner scan = new Scanner(System.in);
+        MAX = scan.nextInt();
+
+        // create an array of PositionApplicant with random values
+        int[] applicants = new int[MAX];
+        for (int i = 0; i < MAX; i++) {
+            applicants[i] = rand.nextInt(1, RANGE + 1);
+        }
+
+        // print the array for the selection sort
+        long startTime = System.nanoTime();
+        PositionApplicant.selectionSort(applicants);
+        long endTime = System.nanoTime();
+        long duration = (endTime - startTime);
+        System.out.println("Selection Sort took " + (duration / 1.e6) + " milliseconds");
+        // System.out.println("Sorted array:" + Arrays.toString(applicants)); 
+
+        // shuffle the array for the quickSort
+        shuffle(applicants);
+        startTime = System.nanoTime();
+        PositionApplicant.quickSort(applicants);
+        endTime = System.nanoTime();
+        duration = (endTime - startTime);
+        System.out.println("Quick Sort took " + (duration / 1.e6) + " milliseconds");
+        
+
+        shuffle(applicants);
+        // print the array using linear search 
+        int randValue = rand.nextInt(RANGE);
+        startTime = System.nanoTime();
+        int index = PositionApplicant.linearSearch(applicants, randValue);
+        endTime = System.nanoTime();
+        duration = (endTime - startTime);
+        // System.out.println("Random index: " + index);
+        System.out.println("Linear Search took " + (duration / 1.e6) + " milliseconds");
+        System.out.println("Linear Search found " + randValue + " at index " + index);
+        
+        shuffle(applicants);
+        // print the sorted array
+        index = PositionApplicant.binarySearch(applicants, randValue);
+        System.out.println("Binary Search took " + (duration / 1.e6) + " milliseconds");
+        System.out.println("Binary search found " + randValue + " at index " + index);
+        
+}
+    // shuffle the array
+    public static void shuffle(int[] applicants) {
+        for (int i = 0; i < applicants.length; i++) {
+            int randomIndexToSwap = (int) (Math.random() * applicants.length);
+           // swap the current element with the random element 
+            int temp = applicants[randomIndexToSwap];
+            applicants[randomIndexToSwap] = applicants[i];
+            applicants[i] = temp;
+        }
+    }
+
+    //Add a static method to swap two elements in an array
+    public static void swap(int[] applicants, int i, int j) {
+        int temp = applicants[i];
+        applicants[i] = applicants[j];
+        applicants[j] = temp;
+    }
+
+    
+}
